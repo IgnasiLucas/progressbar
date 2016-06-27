3230906ea5cace04101a0dad7066c7d7cbeef484
ignasi <ignasilucas@gmail.com>
2016-06-27 20:30:38 +0200

    Added a trap to kill child processes, but does not work well yet.

--- progressbar.sh
+++ progressbar.sh
@@ -7,6 +7,30 @@
 # samples. It usually takes a long time, and I found it useful
 # to estimate how long it will take.
 #
+#
+# This function was taken from https://gist.github.com/criccomini/3786342
+# If stopped while SampleBar.sh was sleeping, 'sleep 1h' is a child process
+# that becomes killed before the parent SampleBar.sh itself is. Thus, the
+# subsequent gawk command gets executed before the function kill_child_processes
+# knowing about it. By the time SampleBar.sh is killed, gawk keeps running.
+
+function kill_child_processes() {
+   isTopmost=$1
+   curPid=$2
+   childPids=`ps -o pid --no-headers --ppid ${curPid}`
+   echo "The following are children of ${curPid}"
+   ps --ppid ${curPid}
+   for childPid in $childPids; do
+      kill_child_processes 0 $childPid
+   done
+   echo "After killing children of ${curPid}, is there any left?"
+   ps --ppid ${curPid}
+   if [ $isTopmost -eq 0 ]; then
+      kill -9 $curPid 2> /dev/null
+   fi
+}
+
+trap "kill_child_processes 1 $$; exit 0" INT
 
 if [ ! -d edits ]; then
    echo "edits folder not found."
@@ -28,6 +52,13 @@ else
 fi
 
 NumSamples=`ls -1 edits/*.derep | wc -l`
+
+# I call a second script to actually print the progress bar, because that
+# way I can easily parallelize the task and generate as many bars as samples
+# are being processed. This script needs to know where it is installed,
+# in order to call the second one. In addition, I need to be able to kill
+# SampleBar and its subprocess (sleep and gawk) if progressbar.sh gets
+# interrupted with Ctrl-C.
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do
